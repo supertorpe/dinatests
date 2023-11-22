39553bb4352c5bb9265bf746f0d81dfed4e3d614
supertorpe <supertorpe@hotmail.com>
2023-11-22 19:59:39 +0100

    Upgrade dependencies + make PWA

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,42 @@
 import { defineConfig } from 'vite';
+import { splitVendorChunkPlugin } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
+  build: {
+    sourcemap: true
+  },
+  plugins: [
+    splitVendorChunkPlugin(),
+    VitePWA({
+      manifestFilename: 'manifest.json',
+      includeAssets: ['*','assets/icons/*','assets/svg/*'],
+      manifest: {
+        name: 'Dinatests',
+        short_name: 'dinatests',
+        description: 'Dinatests',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'assets/icons/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'assets/icons/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'assets/icons/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          }
+        ]
+      }
+    })
+  ],
   server: {
     proxy: {
       '/dinatests/api': {
